f2e338c6b695d05267c360feb5012d3e729965a8
Проскуренко Александр Евгеньевич <gr624_praev@ttit.local>
2023-12-20 10:05:07 +0700

    yra

--- ConsoleApplication1/ConsoleApplication19 4.6/exersize19.cs
+++ ConsoleApplication1/ConsoleApplication19 4.6/exersize19.cs
@@ -2,19 +2,18 @@
 
 double a, b;
 
-Console.WriteLine("Введите координаты точки (a, b):");
+Console.WriteLine("use point coordinates (a, b):");
 Console.Write("a = ");
 a = Convert.ToDouble(Console.ReadLine());
 Console.Write("b = ");
 b = Convert.ToDouble(Console.ReadLine());
 
-// Проверка принадлежности точки заштрихованной области
 if (-2 <= a && a <= 2 && -3 <= b && b <= 0)
 {
-    Console.WriteLine("Точка ({0}; {1}) принадлежит заштрихованной области.", a, b);
+    Console.WriteLine("The point ({0}; {1}) belongs to the shaded area.", a, b);
 }
 else
 {
-    Console.WriteLine("Точка ({0}; {1}) не принадлежит заштрихованной области.", a, b);
+    Console.WriteLine("The point ({0}; {1}) does not belong to the shaded area.", a, b);
 }
 
